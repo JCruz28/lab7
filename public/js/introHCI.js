5b27aadb672f513d2c2cc928a23a041fd20fd948
Justin Cruz <jtc027@ucsd.edu>
2019-02-21 11:06:11 -0800

    first push

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(pushedLike);
+}
+
+function pushedLike(){
+	console.log("In pushedLike function");
+	//ga("create", "UA-134940495-1", "auto");
+	ga("send", "event", "like", "click");
 }
